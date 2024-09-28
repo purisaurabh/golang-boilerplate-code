86bc4301674f3a667658b36bcc5980c0155d9e5c
Saurabh Puri <purisaurabh2885@gmail.com>
2024-09-28 20:25:34 +0530

    add response file

--- internal/api/response.go
+++ internal/api/response.go
@@ -1,1 +1,35 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func SuccessResponse(rw http.ResponseWriter, status int, response interface{}) {
+	respBytes, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error marshalling in success response:", err)
+		status = http.StatusInternalServerError
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(status)
+	rw.Write(respBytes)
+}
+
+func ErrorResponse(rw http.ResponseWriter, status int, response interface{}) {
+	respBytes, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error marshalling in error response:", err)
+		status = http.StatusInternalServerError
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(status)
+	rw.Write(respBytes)
+}
