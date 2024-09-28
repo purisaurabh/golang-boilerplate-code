fab473749fb59bdb8f25e242506dc32e32c3f5b4
Saurabh Puri <purisaurabh2885@gmail.com>
2024-09-28 20:25:42 +0530

    add routes

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,10 +1,18 @@
 package api
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/purisaurabh/database-connection/internal/app"
 )
 
-func Routes(router *mux.Router, svc app.Service) {
+func Routes(ctx context.Context, svc app.Service) *mux.Router {
+	router := mux.NewRouter()
 
+	router.HandleFunc("/post", PostDataApi(ctx, svc)).Methods(http.MethodPost)
+	router.HandleFunc("/list", ListProfileData(ctx, svc)).Methods(http.MethodGet)
+	router.HandleFunc("/profile_update/{id}", UpdateProfileData(ctx, svc)).Methods(http.MethodPut)
+	return router
 }
